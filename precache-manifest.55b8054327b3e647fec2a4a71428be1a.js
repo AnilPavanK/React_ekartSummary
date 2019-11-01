b010ed3fe083543151bbd3029be7a7a0733573be
Anil Pavan <appkumark@yahoo.com>
2019-11-01 15:58:28 +0530

    Updates

---
+++ precache-manifest.55b8054327b3e647fec2a4a71428be1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c59ccfd08578557e295da5de6eb918",
+    "url": "/React_ekartSummary/index.html"
+  },
+  {
+    "revision": "febda24a5d2a430c5e22",
+    "url": "/React_ekartSummary/static/css/main.0b192635.chunk.css"
+  },
+  {
+    "revision": "966aa7abb6ae8c34daba",
+    "url": "/React_ekartSummary/static/js/2.0de1e603.chunk.js"
+  },
+  {
+    "revision": "febda24a5d2a430c5e22",
+    "url": "/React_ekartSummary/static/js/main.9af2b544.chunk.js"
+  },
+  {
+    "revision": "fa1b04624f9c8fd84e1d",
+    "url": "/React_ekartSummary/static/js/runtime-main.69a88bba.js"
+  }
+]);
